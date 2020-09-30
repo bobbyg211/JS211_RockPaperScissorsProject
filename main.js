850aea89b06ef1c56512ecf44ac1d2ae72362d34
bobbyg211 <robertdg96@gmail.com>
2020-09-30 13:46:10 -0500

    Simplify tie logic

--- main.js
+++ main.js
@@ -28,11 +28,7 @@ const rockPaperScissors = (hand1, hand2) => {
     (hand2 === "scissors" && hand1 === "paper")
   ) {
     return "Hand two wins!";
-  } else if (
-    (hand1 === "rock" && hand2 === "rock") ||
-    (hand1 === "paper" && hand2 === "paper") ||
-    (hand1 === "scissors" && hand2 === "scissors")
-  ) {
+  } else if (hand1 === hand2) {
     return "It's a tie!";
   }
 };
